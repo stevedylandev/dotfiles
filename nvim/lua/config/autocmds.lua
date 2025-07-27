56aa6689ebec64f36025fdbf43c2af3c7295d34c
Steve Simkins <stevedsimkins@gmail.com>
2025-07-27 16:27:21 -0400

    chore: Updated neovim config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -73,10 +73,10 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 
-require("conform").setup({
+--[[ require("conform").setup({
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_format = "fallback",
   },
-})
+}) ]]
