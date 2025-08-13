3f0c89f0b6433c6b4287805901036600aa3eb0a5
Steve <steve@terminal.ceo>
2025-08-13 01:31:42 -0400

    chore: Added auto wrapping for md files in neovim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -73,6 +73,14 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "mdx", "text" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true  -- Break at word boundaries
+  end,
+})
+
 --[[ require("conform").setup({
   format_on_save = {
     -- These options will be passed to conform.format()
