474d8d077651de0a15d8cff44e6f89ec5a729791
Steve <steve@terminal.ceo>
2025-08-13 01:24:56 -0400

    chore: Updated neovim config

---
+++ nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,9 @@
+return {
+   "m4xshen/hardtime.nvim",
+   lazy = false,
+   dependencies = { "MunifTanjim/nui.nvim" },
+   opts = {},
+   config = function()
+      require("hardtime").setup()
+   end
+}
