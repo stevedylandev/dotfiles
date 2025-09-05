dbfc52c285b0811f4c567087b324c3dc78e48a81
Steve <contact@stevedylan.dev>
2025-09-05 09:57:49 -0400

    chore: added poimandres

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -6,3 +6,21 @@ return {
 		vim.cmd.colorscheme "darkmatter"
 	end,
 }
+
+-- return {
+--   'olivercederborg/poimandres.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require('poimandres').setup {
+--       -- leave this setup function empty for default config
+--       -- or refer to the configuration section
+--       -- for configuration options
+--     }
+--   end,
+--
+--   -- optionally set the colorscheme within lazy config
+--   init = function()
+--     vim.cmd("colorscheme poimandres")
+--   end
+-- }
