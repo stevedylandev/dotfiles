eb0e2518af1db130e3e7b78f629ac927891d1447
Steve <contact@stevedylan.dev>
2026-03-16 22:06:47 -0400

    chore: updated astro lsp

--- nvim/lsp/astro.lua
+++ nvim/lsp/astro.lua
@@ -1,12 +1,13 @@
 return {
-	cmd = {
-		"astro-ls", "--stdio"
-	},
-	filetypes = {
-		"astro",
-	},
-	single_file_support = true,
-	log_level = vim.lsp.protocol.MessageType.Warning,
+  cmd = {
+    "astro-ls", "--stdio"
+  },
+  filetypes = {
+    "astro",
+  },
+  root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+  single_file_support = true,
+  log_level = vim.lsp.protocol.MessageType.Warning,
   init_options = {
     typescript = {
       tsdk = "/Users/stevedylandev/.bun/install/global/node_modules/typescript/lib"
