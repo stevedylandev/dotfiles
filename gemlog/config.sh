c5a0e19b23e25c391f079e59f7eae3aa4700a93d
Steve <contact@stevedylan.dev>
2026-05-01 07:20:50 -0400

    chore: added gemlog

---
+++ gemlog/config.sh
@@ -0,0 +1,23 @@
+export global_title="gem.stevedylan.dev"
+export global_description="Floating out into space, looking back on the pale blue dot"
+export global_url="gemini://gem.stevedylan.dev/" # link to base url of gemlog
+export global_author="Steve"
+export global_author_email="contact@stevedylan.dev"
+export global_author_url="gemini://gem.stevedylan.dev/" # homepage of author
+export global_license=""
+
+export gemlog_feed="atom.xml" # filename of the atom feed
+export number_of_feed_articles="50" # maximum number of posts added to atom feed
+export feed_base_url="gemini://gem.stevedylan.dev/" # base url that the feed is hosted at
+export feed_web_url="" # base url that the feed is hosted at on the web
+
+export index="gemlog/index.gmi" # main page of gemlog, not recommended to change
+
+export use_year_divider=true # separate posts from different years with a heading?
+
+# these 2 are exclusive, and use_month_divider_nl takes priority if both are set
+export use_month_divider_nl=true # separate posts from different months with a newline?
+export use_month_divider_hd=false # separate posts from different months with a heading?
+
+
+export gemlog_sh_link="https://git.sr.ht/~nytpu/gemlog.sh" # link to the utility, you should change this if you've made substantial non-configuration changes
