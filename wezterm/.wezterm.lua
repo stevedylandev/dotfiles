06e73ddcd993dba61b9fc574d4ec8581e4c55bbd
Steve Simkins <contact@stevedylan.dev>
2026-06-15 17:50:48 -0400

    chore: update linux mapping

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -72,7 +72,7 @@ config.initial_rows = 30
 config.keys = {
   {
     key = 'k',
-    mods = 'CMD',
+    mods = 'SUPER',
     action = wezterm.action.Multiple {
       wezterm.action.SendKey { key = 'q', mods = 'CTRL' },  -- send prefix (C-q)
       wezterm.action.SendKey { key = 'T' },                  -- send T
